d5423af5616221106630bee28a846371e8105364
Egor Reviakin <lolimorimorf@gmail.com>
2024-05-26 13:14:04 +0300

    add new db schema for test

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,12 +10,45 @@ const sqlSchema = generatedSqlSchema.authorization(allow => allow.guest())
 // ])
 .setRelationships((models) => [
   models.orders_ca_short.relationships({
-    reviews: a.hasOne("reviews_data_ca", "index"), //["amazon_order_id", "asin", "sku"]
+    reviews: a.hasOne("reviews_data_ca", ["amazon_order_id", "asin", "sku"]), //primary keys
   }),
   models.reviews_data_ca.relationships({
-    orders: a.belongsTo("orders_ca_short", "index")//["amazon_order_id", "asin", "sku"]
+    orders: a.belongsTo("orders_ca_short", ["amazon_order_id", "asin", "sku"])
   })
-]);
+])
+.setAuthorization((models) => [
+// Model-level authorization rules
+  models.orders_ca_short.authorization((allow) => [allow.publicApiKey(), allow.guest()]),
+  models.reviews_data_ca.authorization((allow) => [allow.publicApiKey(), allow.guest()])
+// Field-level authorization rules
+  // models.orders_ca_short.fields.amazon_order_id.authorization(allow => [allow.publicApiKey(), allow.guest()]),
+  // models.orders_ca_short.fields.asin.authorization(allow => [allow.publicApiKey(), allow.guest()])
+])
+// .addToSchema({
+//   listEventsWithDecodedLatLong: a.query()
+//     // reference custom types added to the schema
+//     .returns(a.ref("EventWithDecodedCoord").array())
+//     .handler(a.handler.inlineSql(
+//         `SELECT
+//           id,
+//           name,
+//           address,
+//           ST_X(geom) AS longitude,
+//           ST_Y(geom) AS latitude
+//         FROM locations;`
+//     ))
+//     .authorization(allow => [allow.guest()]),
+//     // Define custom types to provide end-to-end typed results
+//     // for custom queries / mutations
+//     EventWithDecodedCoord: a.customType({
+//       id: a.integer(),
+//       name: a.string(),
+//       address: a.string(),
+//       longitude: a.float(),
+//       latitude: a.float(),
+//     })
+// })
+
 
 // Relational database sources can coexist with DynamoDB tables managed by Amplify.
 // const schema = a.schema({
