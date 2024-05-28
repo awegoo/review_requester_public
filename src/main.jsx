badf79602cbe36f5419b7fc8c47aab8671c5a507
Sharik25 <72133218+Sharik25@users.noreply.github.com>
2024-05-28 21:03:00 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Authenticator } from '@aws-amplify/ui-react';
-// import { Amplify } from "aws-amplify";
-// import outputs from "../amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 
-// Amplify.configure(outputs);
+Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Authenticator>
