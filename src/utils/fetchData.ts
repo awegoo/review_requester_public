bf742ab79f19a4159892082b412eb64a52c51087
kost90 <kostdonc90@gmail.com>
2024-06-01 10:38:48 +0100

    sync test postgres DB with amplify, created auth by email, created custom query for fetching all reviews from db, created utils folders in front side in this folder created fetch functions

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,24 @@
+import { generateClient } from "aws-amplify/data";
+import { type Schema } from "../../amplify/data/resource";
+
+const client = generateClient<Schema>();
+
+// Function for fetching all existing reviews from database
+export async function getAllReviews() {
+  const { data: items, errors } = await client.queries.listItems();
+  if (errors) {
+    return errors;
+  }
+  return items;
+}
+
+// Function for fetching single review from database by id
+export async function getSingleReview(id: string) {
+  const { data: review, errors } = await client.models.Reviews.get({
+    review_id: id,
+  });
+  if (errors) {
+    return errors;
+  }
+  return review;
+}
