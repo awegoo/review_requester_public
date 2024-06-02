1cb4e5049ff277edd82ff836f7b8be8edc929d42
Sharik25 <72133218+Sharik25@users.noreply.github.com>
2024-06-02 16:10:03 +0300

    Update resource.ts

--- amplify/fetchDataFromDB/resource.ts
+++ amplify/fetchDataFromDB/resource.ts
@@ -2,5 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
     
 export const fetchDataFromDB = defineFunction({
   name: "fetchDataFromDB",
-  entry: " ./handler.ts"
+  entry: "./handler.ts"
 });
