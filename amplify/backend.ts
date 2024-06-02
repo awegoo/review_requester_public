18b67bf4805a8fcb795dfa0cc5af19c001ae1418
Egor Reviakin <lolimorimorf@gmail.com>
2024-06-02 16:00:51 +0300

    updated backend with new function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { myFirstFunction } from './my-first-function/resource'
+import { fetchDataFromDB } from './fetchDataFromDB/resource'
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -9,5 +10,6 @@ import { myFirstFunction } from './my-first-function/resource'
 defineBackend({
   auth,
   data,
-  myFirstFunction
+  myFirstFunction,
+  fetchDataFromDB
 });
