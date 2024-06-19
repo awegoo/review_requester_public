51ca7ffe68ec20c42146ecf8349c5a1fb8cfb90a
Egor Reviakin <lolimorimorf@gmail.com>
2024-06-19 18:38:12 +0300

    minor frontend adjustments

--- amplify/functions/getAccessToken/resource.ts
+++ amplify/functions/getAccessToken/resource.ts
@@ -5,9 +5,9 @@ export const getAccessToken = defineFunction({
     entry:'./handler.ts',
     // ! for dev
     environment:{
-        REFRESH_TOKEN: import.meta.env.VITE_LWA_REFRESH_TOKEN,
-        LWA_APP_ID: import.meta.env.VITE_LWA_APP_ID,
-        LWA_CLIENT_SECRET: import.meta.env.VITE_LWA_CLIENT_SECRET,
-        BASE_URL:import.meta.env.VITE_BASE_URL,
+        REFRESH_TOKEN: 'Atzr|IwEBIM6pfjjXGEPDhe0Y23Bo0tmLzTsxj3H829DchYPesoM9dea3d7IUUUAjLgqJRY_dmDvlEEw8YHr-gbsCQi7zE-lvSbAtPhdjD5Ph4rLa_rIZHGPobdWTcFM-WiHH8gYtcOfh_N1lDShAJoJ5MVrJjPx6F8nCL9Ez5-Y3RlO0Whps_MnUVt6oajMI_PV4E0U9SbUDyoN50M7S6IuqLrsD4K5Jfmyc-YAaUcD37j99_o99lZGROHS8fF3QTshig06q7y9Wm5m0fcBBc87cLXOSOZf4HBD8wEzlkc7Om9I9Hh_GvW8AvSCfZa7hBy38s7zZKeMbjDS-pcM0T-_2ehKsJUVN', //import.meta.env.VITE_LWA_REFRESH_TOKEN,
+        LWA_APP_ID: 'amzn1.application-oa2-client.153a3f456f2543fcbbe724ea21baa1c9', //import.meta.env.VITE_LWA_APP_ID,
+        LWA_CLIENT_SECRET: "amzn1.oa2-cs.v1.a23507f4cad48378cba3d1be18d3da4079ca6f9104094624af519f8f1c92af75", //import.meta.env.VITE_LWA_CLIENT_SECRET,
+        BASE_URL: 'https://api.amazon.com/auth/o2/token' //import.meta.env.VITE_BASE_URL,
     }
 })
--- src/utils/generatedData.js
+++ src/utils/generatedData.js
@@ -2,8 +2,8 @@ const generateRandomData = () => {
     const data = [];
     for (let day = 1; day <= 30; day++) {
       const requests = Math.floor(Math.random() * (1000 - 700 + 1)) + 700;
-      const eligible = requests - Math.floor(Math.random() * (100 - 50 + 1)) + 50;
-      data.push({ day, requests, eligible });
+      const orders = requests - Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+      data.push({ day, requests, orders });
     }
     return data;
   };
