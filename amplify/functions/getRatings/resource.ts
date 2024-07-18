efdb67d8178de4a0be5a520290f753e5fe500517
kost90 <kostdonc90@gmail.com>
2024-07-18 22:46:36 +0100

    edited getRatings function, added getRtings function to the backend file

--- amplify/functions/getRatings/resource.ts
+++ amplify/functions/getRatings/resource.ts
@@ -4,8 +4,8 @@ export const getRatings = defineFunction({
   name: "getRatings",
   entry: "./handler.ts",
   environment: {
-    URL: 'https://www.amazon.ca/sp?marketplaceID=A2EUQ1WTGCTBG2&seller=A1UN6Z7VPU7RJG',  //import.meta.env.VITE_URL,
-    SELLER_ID:  "A1UN6Z7VPU7RJG", //import.meta.env.VITE_SELLER_ID,
-    STRING_CONNECTION: "postgres://postgres:X8763590y@review-requester-test.cy87iea7j6qh.ca-central-1.rds.amazonaws.com/postgres", //import.meta.env.VITE_CONNECTION_STRING,
+    URL: import.meta.env.VITE_URL,
+    SELLER_ID:  import.meta.env.VITE_SELLER_ID,
+    STRING_CONNECTION: import.meta.env.VITE_CONNECTION_STRING,
   },
 });
